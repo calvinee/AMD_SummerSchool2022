318cb0486f9633f236de48177252135749ce7aae
calvin <35540888+calvinee@users.noreply.github.com>
2022-08-10 14:44:57 +0800

    Add files via upload

---
+++ src/tcl_script.tcl
@@ -0,0 +1,25 @@
+set Project     cordiccart2pol
+set Solution    solution1
+set Device      "xc7z020clg400-1"
+set Flow        "vivado"
+set Clock       10
+
+open_project $Project -reset
+
+set_top cordiccart2pol
+
+add_files cordiccart2pol.cpp -cflags -I.
+add_files cordiccart2pol.h -cflags -I. 
+add_files -tb cordiccart2pol_test.cpp -cflags -I.
+
+open_solution -flow_target $Flow -reset $Solution
+set_part $Device
+create_clock -period $Clock
+
+csim_design
+csynth_design
+cosim_design
+
+export_design -rtl verilog -format ip_catalog -flow syn
+
+exit
