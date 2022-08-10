acd5abd96d85a67da20a948da256ddf40af3c76f
calvin <35540888+calvinee@users.noreply.github.com>
2022-08-10 14:39:48 +0800

    Update cordiccart2pol.cpp

--- cordiccart2pol.cpp
+++ cordiccart2pol.cpp
@@ -21,12 +21,11 @@ data_t cordic_angles[NO_ITER] = {
 void cordiccart2pol(data_t x, data_t y, data_t * r,  data_t * theta)
 {
     // Write your code here
-	#pragma HLS INTERFACE s_axilite port=x  bundle=BUS_A
-	#pragma HLS INTERFACE s_axilite port=y  bundle=BUS_A
-	#pragma HLS INTERFACE s_axilite port=r  bundle=BUS_A
-	#pragma HLS INTERFACE s_axilite port=theta  bundle=BUS_A
-	//return绔彛寰堥噸瑕侊紝瀹氫箟鍚庢墠鑳藉湪block_design杩炴帴涓奱p_ctrl锛屼笌zynq鐨刟xilite鎵嶈兘鎻℃墜
-	#pragma HLS INTERFACE s_axilite port=return
+	#pragma HLS INTERFACE m_axi port=x  depth=64
+	#pragma HLS INTERFACE m_axi port=y  depth=64
+	#pragma HLS INTERFACE m_axi port=r  depth=64
+	#pragma HLS INTERFACE m_axi port=theta   depth=64
+	#pragma HLS INTERFACE s_axilite port=return bundle=CTRL
 
 	//decalaration
 	const data_t Kvalues_compensate = 0.6072529350088814;
@@ -50,7 +49,7 @@ void cordiccart2pol(data_t x, data_t y, data_t * r,  data_t * theta)
 		angle = -pi_half;
 	}
 
-#pragma HLS PIPELINE II =4
+#pragma HLS PIPELINE
 LOOP_X:for (int i = 0; i < NO_ITER; i++) {
 		if (c_y < 0) {
 			signma = 1;
